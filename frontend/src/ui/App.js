24a0de2408e0f4bd6df53aca0de05cee326f86f7
Stephen Wade <swade6@cnm.edu>
2022-07-07 15:45:20 -0600

    spining up react-app-practice project

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,17 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-import { Home } from '../../../../pothole-finder/frontend/src/ui/Home'
-import { FourOhFour } from '../../../../../../CNM DeepDive Bootcamp/git/pothole-finder/frontend/src/ui/FourOhFour'
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
 
-export const App = () => (
-	<>
-		<BrowserRouter>
-			<Routes>
-				<Route  path='/' element={<Home />} />
-				<Route path="*" element={<FourOhFour />} />
-			</Routes>
-		</BrowserRouter>
-
-	</>
-)
+export const App = () => ( <h1 className="text-info">Is this thing on?</h1> );
